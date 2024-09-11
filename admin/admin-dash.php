a479f34602d686d48c90d26ea63361ae65093b67
Kabir <kabirajibade@yahoo.com>
2024-09-11 17:11:14 +0100

    Added admin Css

---
+++ admin/admin-dash.php
@@ -0,0 +1,36 @@
+<?php
+$title="Farmers Crib ::Admin";
+    require_once "adminHeader.php";
+?>
+
+<main class="row container-fluid">
+<div class="container-fluid col-12">
+            <h1 class="mt-4">Admin Dashboard</h1>
+            <p>Welcome to the admin panel. Use the sidebar to navigate through the options.</p>
+        </div>
+    <!-- Page Content -->
+<section class="col-md-3 col-sm-3 sidenav">
+            <?php
+                require_once "navAdmin.php";
+            ?>
+
+</section>
+    
+    <main class="col-md-9 col-sm-9 row" id="admin-details">
+        <div class="admin-charts col-4">
+        <h4>Labourers</h4>
+
+        </div>
+        <div class="admin-charts">
+            <h4>Users</h4>
+
+        </div>
+        <div class="admin-charts">
+            <h4>Hires</h4>
+        </div>
+    </main>
+
+</main>
+<?php 
+require_once "adminFooter.php";
+?>
--- admin/navAdmin.php
+++ admin/navAdmin.php
@@ -1,14 +1,14 @@
-<!-- Sidebar -->
-<div class="d-flex" id="wrapper">
-    <div class="bg-light border-right" id="sidebar-wrapper">
-        <div class="sidebar-heading">Admin Dashboard</div>
-        <div class="list-group list-group-flush">
-            <a href="#" class="list-group-item list-group-item-action bg-light">Manage Users</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Manage Workers</a>
-            <!-- <a href="#" class="list-group-item list-group-item-action bg-light">Transactions</a> -->
-            <a href="#" class="list-group-item list-group-item-action bg-light">Settings</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Logout</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Login</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Register Admin</a>
-        </div>
-    </div>
+<?php require_once "adminHeader.php" ?>
+<i class="fa fa-user text-success" aria-hidden="true"></i>
+<h2 class="text-white">Admin</h2>
+        <ul>
+            <li><a class="link" onclick="bghover()" href="index.php"><i class="fas fa-home" aria-hidden="true"></i>Home</a></li>
+            <li><a class="link" href="admin-dash.php"><i class="fa fa-dashboard" aria-hidden="true"></i>Dashboard</a></li>
+            <li><a class="link" href="add_labourers.php"><i class="fa fa-cart-plus" aria-hidden="true"></i>Add Product</a></li>
+            <li><a class="link" href="labourers.php"><i class="fa fa-shopping-basket" aria-hidden="true"></i>Products</a></li>
+            <li><a class="link" href=""><i class="fa fa-user-circle" aria-hidden="true"></i>Users</a></li>
+            <li><a class="link" href="add_user.php"><i class="fa fa-user-circle" aria-hidden="true"></i>Add user</a></li>
+            <li><a class="link" href=""><i class="fa fa-pie-chart" aria-hidden="true"></i>Statistics</a></li>
+            <li><a class="link" href=""><i class="fa fa-sign-in" aria-hidden="true"></i>Login</a></li>
+            <li><a class="link" href=""><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</a></li>
+        </ul>
